b2745bf9245f13a3bccd08505faa89a85d7e48f5
Ashish <105300000+ShadYBhai@users.noreply.github.com>
2022-07-11 15:51:19 +0530

    Update counter-app.js

--- counter-app.js
+++ counter-app.js
@@ -1,26 +1,42 @@
-let count=0
+class Counter extends React.Component{
+    constructor(props){
+        super(props)
+        this.handelOne =this.handelOne.bind(this)
+        this.handelMinus =this.handelMinus.bind(this)
+        this.handelReset = this.handelReset.bind(this)
+        this.state = {
+            count : 0
+        }
+    }
 
-const minus = () =>{
-    count --;
-    counterApp()
-}
-const reset = () => {
-    count = 0
-    counterApp()
-}
-const plus = () =>{
-    count++
-    counterApp()
-}
-const counterApp= () =>{
-    const templateTwo = (
-        <div>
-            <h1>Counter : {count}</h1>
-            <button onClick={plus}>+1</button>
-            <button onClick={minus}>-1</button>
-            <button onClick={reset}>Reset</button>
+    handelOne(){
+        this.setState((prevState)=>{
+            return {
+                count: prevState.count+1
+            }
+        })
+    }
+    handelMinus(){
+        this.setState((prevState) =>{//setState is a react property to operate that we need to bind Our fns to this. context.
+            return {
+                count: prevState.count-1
+            }
+        })
+    }handelReset(){
+        this.setState((prevState)=>{
+            return {
+                count : prevState=0
+            }
+        })
+    }
+    render(){
+        return (<div>
+            <h1>Count: {this.state.count} </h1>
+            <button onClick={this.handelOne}>+1</button>
+            <button onClick={this.handelMinus}>-1</button>
+            <button onClick={this.handelReset}>Reset</button>
         </div>)
-
-        ReactDOM.render(templateTwo, root)
+    }
 }
-counterApp()
+        const root = ReactDOM.createRoot(document.getElementById('app'))
+        root.render(<Counter/>)
